4039c7ad661354322965ac7a3213361fd009f3d9
hslee <hslee@salin.co.kr>
2024-01-11 17:18:44 +0900

    [feat] login, main page, sidebar, etc

---
+++ src/constants/menuItems.tsx
@@ -0,0 +1,12 @@
+export const MenuItems = [
+    { name: '홈', link: '/', icon: '/assets/icon-home-regular.svg' },
+    { name: '검색', link: '/search', icon: '/assets/icon-search-regular.svg' },
+    { name: '탐색 탭', link: '/explore', icon: '/assets/icon-explore-regular.svg' },
+    { name: '릴스', link: '/reels', icon: '/assets/icon-reels-regular.svg' },
+    { name: '메시지', link: '/message', icon: '/assets/icon-messenger-regular.svg' },
+    { name: '알림', link: '/', icon: '/assets/icon-notification-regular.svg' },
+    { name: '만들기', link: '/', icon: '/assets/icon-new-post-regular.svg' },
+    { name: '프로필', link: '/profile', icon: '/assets/default_profile.svg' },
+]
+
+
